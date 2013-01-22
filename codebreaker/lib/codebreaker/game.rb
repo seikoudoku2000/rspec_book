b7bf9e51c85c7db20eec3d0e353d10b13feee830
Yosuke Tomita <seikoudoku777@gmail.com>
2013-01-23 01:30:04 +0900

    chapter 7-1, 7-2

--- codebreaker/lib/codebreaker/game.rb
+++ codebreaker/lib/codebreaker/game.rb
@@ -7,10 +7,26 @@ module Codebreaker
 		def start(secret)
 			@output.puts 'Welcome to Codebreaker!'
 			@output.puts 'Enter guess:'
+			@secret = secret
 		end
 
 		def guess(guess)
-			
+			if extra_match?(guess, 0)
+				mark = '+'
+			elsif number_match?(guess, 0)
+				mark = '-'
+			else 
+				mark = ''
+			end
+			@output.puts mark
+		end
+
+		def extra_match?(guess, index)
+			guess[index] == @secret[index]
+		end
+
+		def number_match?(guess, index)
+			@secret.include?(guess[index])
 		end
 	end
 end
--- codebreaker/spec/codebreaker/game_spec.rb
+++ codebreaker/spec/codebreaker/game_spec.rb
@@ -2,10 +2,10 @@ require 'spec_helper'
 
 module Codebreaker
 	describe Game do
-		describe "#start" do
-			let(:output) {double('output').as_null_object}
-			let(:game) {Game.new(output)}
+		let(:output) {double('output').as_null_object}
+		let(:game) {Game.new(output)}
 
+		describe "#start" do	
 			it "sends a welcome message" do
 				output.should_receive(:puts).with('Welcome to Codebreaker!')
 				game.start('1234')
@@ -16,5 +16,37 @@ module Codebreaker
 				game.start('1234')
 			end
 		end
+
+		describe "#guess" do
+			context "with no matches" do
+				it "sends a mark with ''" do
+					game.start('1234')
+					output.should_receive(:puts).with('')
+					game.guess('5555')
+				end
+			end
+			context "with 1 number match" do
+				it "sends a mark with ''" do
+					game.start('1234')
+					output.should_receive(:puts).with('-')
+					game.guess('2555')
+				end
+			end
+			context "with 1 extra match" do
+				it "sends a mark with ''" do
+					game.start('1234')
+					output.should_receive(:puts).with('+')
+					game.guess('1555')
+				end
+			end
+			context "with 2 number match" do
+				it "sends a mark with ''" do
+					game.start('1234')
+					output.should_receive(:puts).with('--')
+					game.guess('2355')
+				end
+			end
+		end
+
 	end
 end
